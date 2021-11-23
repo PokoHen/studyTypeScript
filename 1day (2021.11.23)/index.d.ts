72b441b478537f08fd3e193ded7e76ef58ebc7dc
PokoHen <46313883+PokoHen@users.noreply.github.com>
2021-11-23 18:55:25 +0900

    Add files via upload

---
+++ 1day (2021.11.23)/index.d.ts
@@ -0,0 +1,34 @@
+declare let 이름: string[];
+declare let 이름2: {
+    name?: string;
+};
+declare let 이름3: string | number;
+declare type 내타입 = string | number;
+declare let 이름4: 내타입;
+declare function 함수(x: number): number;
+declare type Member = [number, boolean];
+declare let john: Member;
+declare type Member2 = {
+    name: string;
+    [key: string]: string;
+};
+declare let john2: Member2;
+declare class User {
+    name: string;
+    constructor(name: string);
+}
+declare let test: string;
+declare let my_name: string;
+declare let my_age: number;
+declare let my_country: string;
+declare type song_type = {
+    singer: string | number;
+    song: string | number;
+};
+declare let my_song: song_type;
+declare type my_project = {
+    member: string[];
+    days: number;
+    started: boolean;
+};
+declare let project: my_project;
---
+++ 1day (2021.11.23)/index.js
@@ -0,0 +1,28 @@
+"use strict";
+let 이름 = ['kim', 'park'];
+let 이름2 = { name: 'kinm', };
+let 이름3 = 123;
+let 이름4 = 123;
+function 함수(x) {
+    return x * 2;
+}
+let john = [123, true];
+let john2 = { name: 'kim', };
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
+let test = "text";
+let my_name = "jinhyeon";
+let my_age = 28;
+let my_country = "TongYeong";
+let my_song = {
+    singer: '윤하',
+    song: '우산'
+};
+let project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
