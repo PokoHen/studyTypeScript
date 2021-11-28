3d6b555fc3e353ea898be6b1edbb9b8303754e47
pokohen <canzoo@naver.com>
2021-11-28 22:25:48 +0900

    2day(21.11.28)

---
+++ 2day (2021.11.28)/index.d.ts
@@ -0,0 +1,25 @@
+declare let 회원: number | string;
+declare let 회원들: (number | string)[];
+declare let 오브젝트: {
+    a: (number | string);
+};
+declare let 애니: any;
+declare let 언노운: unknown;
+declare let test: number;
+declare let 나이: string | number;
+declare let user: string;
+declare let age_day2: unknown | number;
+declare let married: boolean;
+declare let 철수: (string | boolean | number | unknown)[];
+declare let 학교: {
+    score: (number | boolean)[];
+    teacher: string;
+    friend: string | string[];
+};
+declare function 함수(x: number): number;
+declare function 함수보이드(x: number): void;
+declare function 함수파라미터(x?: number): void;
+declare function 함수유닝온(x: number | string): void;
+declare function 인사(x?: string): string;
+declare function 글자수(x: number | string): number;
+declare function 결혼가능하냐(x: number, y: boolean, z: string): string | void;
